cb1d181a2185ce87e651f2e1df3a5233f604734f
Derek Mwachinga <mwangome@hotmail.com>
2022-09-09 20:07:55 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,135 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Coming Soon 1</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="https://colorlib.com/etc/cs/comingsoon_01/images/icons/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_01/vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_01/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_01/vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_01/vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_01/css/util.css">
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_01/css/main.css">
+
+<meta name="robots" content="noindex, follow">
+<script nonce="ea2d8dec-c69a-4025-bfb9-e76414f6b5d6">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{};a.zarazData.executed=[];a.zaraz={deferred:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","ecommerce","debug"])a.zaraz[e]=a.zaraz._f(e);a.zaraz.init=()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text);a.zarazData.x=Math.random();a.zarazData.w=a.screen.width;a.zarazData.h=a.screen.height;a.zarazData.j=a.innerHeight;a.zarazData.e=a.innerWidth;a.zarazData.l=a.location.href;a.zarazData.r=e.referrer;a.zarazData.k=a.screen.colorDepth;a.zarazData.n=e.characterSet;a.zarazData.o=(new Date).getTimezoneOffset();a.zarazData.q=[];for(;a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e||{}).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin";z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData)));t.parentNode.insertBefore(z,t)};["complete","interactive"].includes(e.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
+<body>
+<div class="size1 bg0 where1-parent">
+
+<div class="flex-c-m bg-img1 size2 where1 overlay1 where2 respon2" style="background-image: url('https://colorlib.com/etc/cs/comingsoon_01/images/bg01.jpg');">
+<div class="wsize2 flex-w flex-c-m cd100 js-tilt">
+<div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+<span class="l2-txt1 p-b-9 days">35</span>
+<span class="s2-txt4">Days</span>
+</div>
+<div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+<span class="l2-txt1 p-b-9 hours">17</span>
+<span class="s2-txt4">Hours</span>
+</div>
+<div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+<span class="l2-txt1 p-b-9 minutes">50</span>
+<span class="s2-txt4">Minutes</span>
+</div>
+<div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+<span class="l2-txt1 p-b-9 seconds">39</span>
+<span class="s2-txt4">Seconds</span>
+</div>
+</div>
+</div>
+
+<div class="size3 flex-col-sb flex-w p-l-75 p-r-75 p-t-45 p-b-45 respon1">
+<div class="wrap-pic1">
+<img src="https://play-lh.googleusercontent.com/CeRSVUTYKUjxKKdLGLBaKBe4VXjMc7gdsQC4GwgyUXZQnLcLI__fGG4M17T5M1lk1xsK=w240-h480-rw" alt="LOGO">
+</div>
+<div class="p-t-50 p-b-60">
+<p class="m1-txt1 p-b-36">
+Our website is <span class="m1-txt2">Coming Soon</span>, follow us for update now!
+</p>
+<form class="contact100-form validate-form">
+<div class="wrap-input100 m-b-10 validate-input" data-validate="Name is required">
+<input class="s2-txt1 placeholder0 input100" type="text" name="name" placeholder="Your Name">
+<span class="focus-input100"></span>
+</div>
+<div class="wrap-input100 m-b-20 validate-input" data-validate="Email is required: ex@abc.xyz">
+<input class="s2-txt1 placeholder0 input100" type="text" name="email" placeholder="Email Address">
+<span class="focus-input100"></span>
+</div>
+<div class="w-full">
+<button class="flex-c-m s2-txt2 size4 bg1 bor1 hov1 trans-04">
+Subscribe
+</button>
+</div>
+</form>
+<p class="s2-txt3 p-t-18">
+And don’t worry, we hate spam too! You can unsubcribe at anytime.
+</p>
+</div>
+<div class="flex-w">
+<a href="#" class="flex-c-m size5 bg3 how1 trans-04 m-r-5">
+<i class="fa fa-facebook"></i>
+</a>
+<a href="#" class="flex-c-m size5 bg4 how1 trans-04 m-r-5">
+<i class="fa fa-twitter"></i>
+</a>
+<a href="#" class="flex-c-m size5 bg5 how1 trans-04 m-r-5">
+<i class="fa fa-youtube-play"></i>
+</a>
+</div>
+</div>
+</div>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/jquery/jquery-3.2.1.min.js"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/bootstrap/js/popper.js"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/select2/select2.min.js"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/countdowntime/moment.min.js"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/countdowntime/moment-timezone.min.js"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/countdowntime/moment-timezone-with-data.min.js"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/countdowntime/countdowntime.js"></script>
+<script>
+		$('.cd100').countdown100({
+			/*Set Endtime here*/
+			/*Endtime must be > current time*/
+			endtimeYear: 0,
+			endtimeMonth: 0,
+			endtimeDate: 35,
+			endtimeHours: 18,
+			endtimeMinutes: 0,
+			endtimeSeconds: 0,
+			timeZone: "" 
+			// ex:  timeZone: "America/New_York"
+			//go to " http://momentjs.com/timezone/ " to get timezone
+		});
+	</script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_01/vendor/tilt/tilt.jquery.min.js"></script>
+<script>
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_01/js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"748151323ca4051d","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.8.1","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
